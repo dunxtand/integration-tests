09ef5a2dff5c2ffcda70c9e2530f2c7a4feda6c0
dunxtand <mdrstandish@gmail.com>
2017-01-05 16:05:03 -0500

    minor edits

--- test/AU/translation_file_test.js
+++ test/AU/translation_file_test.js
@@ -10,14 +10,12 @@ const pagesToCheck = [
 const { translationMissing } = require("../../values/general")
 
 describe(`no missing text at ${baseUrl} for:`, () => {
-
   pagesToCheck.forEach(urlSegment => {
     let url = baseUrl + urlSegment
     it(`${urlSegment}`, () => {
       browser.url(url)
-      let text = browser.getText("body")
+      let text = browser.getText("body").toLowerCase()
       expect(text.includes(translationMissing)).to.equal(false)
     })
   })
-
 })
