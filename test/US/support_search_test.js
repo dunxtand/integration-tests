b2724636c52a1e1c69a55cb95c02c9e6849313fa
dunxtand <mdrstandish@gmail.com>
2017-01-09 17:43:53 -0500

    slight refactoring of search test

--- test/US/support_search_test.js
+++ test/US/support_search_test.js
@@ -5,24 +5,30 @@ const {
   resultsList, hasResults, noResults
 } = require("../../values/supportPageSelectors").search
 
-describe("support page search functionality", () => {
+describe(`support page functionality for ${baseUrl}`, () => {
   browser.url(baseUrl + urlSegment)
 
-  it("returns a message when there are no results", () => {
-    let noMatchesSearch = "abcdefghijkl"
-    browser.setValue(searchInput, noMatchesSearch)
-    browser.submitForm(searchForm)
-    browser.pause(1000)
-    expect(browser.isExisting(noResults)).to.equal(true)
-    expect(browser.isExisting(hasResults)).to.equal(false)
+  describe("article search", () => {
+    it("returns a message when there are no results", () => {
+      let noMatchesSearch = "abcdefghijkl"
+      browser.setValue(searchInput, noMatchesSearch)
+      browser.submitForm(searchForm)
+      browser.pause(1000)
+      expect(browser.isExisting(noResults)).to.equal(true)
+      expect(browser.isExisting(hasResults)).to.equal(false)
+    })
+
+    it("returns results when there are search parameter matches", () => {
+      let hasMatchesSearch = "hickies"
+      browser.setValue(searchInput, hasMatchesSearch)
+      browser.submitForm(searchForm)
+      browser.pause(1000)
+      expect(browser.isExisting(resultsList)).to.equal(true)
+      expect(browser.isExisting(noResults)).to.equal(false)
+    })
   })
 
-  it("returns results when there are search parameter matches", () => {
-    let hasMatchesSearch = "hickies"
-    browser.setValue(searchInput, hasMatchesSearch)
-    browser.submitForm(searchForm)
-    browser.pause(1000)
-    expect(browser.isExisting(resultsList)).to.equal(true)
-    expect(browser.isExisting(noResults)).to.equal(false)
+  describe("ticket submission", () => {
+    // coming soon
   })
 })
