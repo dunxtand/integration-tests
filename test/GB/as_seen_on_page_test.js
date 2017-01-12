5253082a5633c32210ac28054591344e9dde0261
dunxtand <mdrstandish@gmail.com>
2017-01-12 15:24:07 -0500

    increase timeout limits for as seen on tests

--- test/GB/as_seen_on_page_test.js
+++ test/GB/as_seen_on_page_test.js
@@ -3,15 +3,16 @@ const urlSegment = require("../../values/pageUrls").asSeenOn
 const { articleLink, viewTheLookLink } = require("../../values/asSeenOnPageSelectors")
 const { notFound } = require("../../values/general")
 
-describe(`As Seen On page links for ${baseUrl}`, () => {
+describe(`As Seen On page links for ${baseUrl}`, function () {
   browser.url(baseUrl + urlSegment)
 
-  describe("publication feature links", () => {
+  describe("publication feature links", function () {
     // figure out how to test whether the external links
     // are live articles (non-404)
   })
 
-  describe("celebrity 'view the look' links", () => {
+  describe("celebrity 'view the look' links", function () {
+    this.timeout(40000)
 
     it("redirect to live products", () => {
       let elements = browser.elements(viewTheLookLink).value
